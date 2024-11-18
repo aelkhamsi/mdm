ee82ed640c50807a8ff332d05d3b283a8eb0b5a2
Achraf <achrafelkhamsi@gmail.com>
2024-11-18 16:09:57 +0100

    modify homepage layout

--- apps/front/app/components/home/component-grid.tsx
+++ apps/front/app/components/home/component-grid.tsx
@@ -5,6 +5,7 @@ import { useDemoModal } from "@/app/components/home/demo-modal";
 import Popover from "@/components/shared/popover";
 import Tooltip from "@/components/shared/tooltip";
 import { ChevronDown } from "lucide-react";
+import { Button } from 'ui';
 
 export default function ComponentGrid() {
   const { DemoModal, setShowDemoModal } = useDemoModal();
@@ -12,6 +13,8 @@ export default function ComponentGrid() {
   return (
     <div className="grid grid-cols-1 gap-5 md:grid-cols-3">
       <DemoModal />
+      <Button variant="default">Outline</Button>
+
       <button
         onClick={() => setShowDemoModal(true)}
         className="flex w-36 items-center justify-center rounded-md border border-gray-300 px-3 py-2 transition-all duration-75 hover:border-gray-800 focus:outline-none active:bg-gray-100"
