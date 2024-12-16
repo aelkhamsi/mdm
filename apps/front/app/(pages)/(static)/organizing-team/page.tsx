d261ba7aec798f12eaf6d3e7ab406732861873a2
Achraf <achrafelkhamsi@gmail.com>
2024-12-16 21:22:02 +0100

    update organizing-team page & second section of homepage

--- apps/front/app/(pages)/(static)/organizing-team/page.tsx
+++ apps/front/app/(pages)/(static)/organizing-team/page.tsx
@@ -24,7 +24,7 @@ const Card = ({
 }) => {
   return (
     <div 
-      className="max-w-[10rem] bg-white border-b-4 border-red-500 flex flex-col justify-center items-center space-y-4 rounded-md py-2"
+      className="w-[12rem] bg-white border-b-4 border-red-500 flex flex-col justify-between items-center space-y-2 rounded-md py-2"
       key={key}
     > 
       <div className="h-fit">
@@ -34,8 +34,8 @@ const Card = ({
         />
       </div>
 
-      <div className='font-semibold flex space-x-2 bg-gradient-to-br from-black to-stone-600 inline-block text-transparent bg-clip-text'>
-        <div className='text-xs text-center'>{name}</div>
+      <div className='flex space-x-2'>
+        <div className='text-xs text-center font-semibold '>{name}</div>
         {linkedinSrc && <div><Link href={linkedinSrc} target='_blank' className='shadow-md'><Linkedin className="h-5 w-5 text-[#f04b5b]" /></Link></div>}
         {portfolioSrc && <div><Link href={portfolioSrc} target='_blank' className='shadow-md'><Brush className="h-6 w-6 text-[#f04b5b]" /></Link></div>}
       </div>
@@ -47,26 +47,14 @@ export default function OrganizingTeamPage() {
   const shuffledOrganizingCommitte = shuffle(organizingCommittee)
 
   return (
-    <div className="w-full max-w-sm md:max-w-screen-lg px-5 xl:px-0 mt-10">
-      <div className="space-y-6">
-        <h1
-          className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-2xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-4xl md:leading-[4rem]"
-          style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
-        >
-          <span className='mb-8 bg-gradient-to-br from-sky-500 to-[#272162] inline-block text-transparent bg-clip-text'>Organizing Team</span>
-        </h1>
-
-        <h1
-          className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-3xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-3xl md:leading-[4rem]"
-          style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
-        >
+    <div className="w-full max-w-sm md:max-w-screen-lg px-5 xl:px-0 mt-10 space-y-6">
+        <h1 className="text-center text-3xl font-bold drop-shadow-sm">
           <span className='bg-gradient-to-br from-sky-800 to-[#272162] inline-block text-transparent bg-clip-text'>Math&Maroc</span> <span className='font-extralight'>{" | "}</span>
           <span className='bg-gradient-to-br from-black to-stone-500 inline-block text-transparent bg-clip-text'>Organizing Committee</span>
         </h1>
 
         <div 
-          className="flex justify-around flex-wrap gap-6 shadow-md p-8 rounded-lg animate-fade-up opacity-0 md:gap-x-12"
-          style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
+          className="flex justify-around flex-wrap gap-6 shadow-md p-8 rounded-lg md:gap-x-12"
         >
           {shuffledOrganizingCommitte.map(person =>
             <Card
@@ -79,18 +67,15 @@ export default function OrganizingTeamPage() {
         </div>
 
         <div className='flex flex-col justify-around space-y-6 flex-wrap gap-y-6 md:flex-row md:space-y-0'>
+          {/* STAFF */}
           <div className='w-full md:w-1/2 space-y-4'>
-            <h1
-              className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-2xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-3xl md:leading-[4rem]"
-              style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
-            >
+            <h1 className="text-center text-3xl font-bold drop-shadow-sm">
               <span className='bg-gradient-to-br from-sky-800 to-[#272162] inline-block text-transparent bg-clip-text'>Math&Maroc</span> <span className='font-extralight'>{" | "}</span>
               <span className='bg-gradient-to-br from-black to-stone-500 inline-block text-transparent bg-clip-text'>Staff</span>
             </h1>
             
             <div 
-              className="flex justify-around flex-wrap gap-6 shadow-md p-8 rounded-lg animate-fade-up opacity-0 mr-6"
-              style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
+              className="flex justify-around flex-wrap gap-6 shadow-md p-8 rounded-lg mr-6"
             >
               {staff.map(person =>
                 <Card
@@ -102,19 +87,16 @@ export default function OrganizingTeamPage() {
               )}
             </div>
           </div>
-
+          
+          {/* WEB DEV */}
           <div className='w-full md:w-1/2 space-y-4'>
-            <h1
-              className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-2xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-3xl md:leading-[4rem]"
-              style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
-            >
+            <h1 className="text-center text-3xl font-bold drop-shadow-sm">
               <span className='bg-gradient-to-br from-sky-800 to-[#272162] inline-block text-transparent bg-clip-text'>Math&Maroc</span> <span className='font-extralight'>{" | "}</span>
-              <span className='bg-gradient-to-br from-black to-stone-500 inline-block text-transparent bg-clip-text'>Website Development</span>
+              <span className='bg-gradient-to-br from-black to-stone-500 inline-block text-transparent bg-clip-text'>Web Development</span>
             </h1>
 
             <div 
-              className="flex justify-around flex-wrap gap-6 shadow-md  p-8 rounded-lg animate-fade-up opacity-0 ml-6"
-              style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
+              className="flex justify-around flex-wrap gap-6 shadow-md  p-8 rounded-lg ml-6"
             >
               {webDevelopment.map(person =>
                 <Card
@@ -127,19 +109,16 @@ export default function OrganizingTeamPage() {
               
             </div>
           </div>
-
+          
+          {/* BRANDING & DESIGN */}
           <div className='w-full md:w-1/2 space-y-4'>
-            <h1
-              className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-3xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-3xl md:leading-[4rem]"
-              style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
-            >
+            <h1 className="text-center text-3xl font-bold drop-shadow-sm">
               <span className='bg-gradient-to-br from-sky-800 to-[#272162] inline-block text-transparent bg-clip-text'>Math&Maroc</span> <span className='font-extralight'>{" | "}</span>
-              <span className='bg-gradient-to-br from-black to-stone-500 inline-block text-transparent bg-clip-text'>Branding & Design</span> 
+              <span className='bg-gradient-to-br from-black to-stone-500 inline-block text-transparent bg-clip-text'>Branding & Design</span>
             </h1>
 
             <div 
-              className="flex justify-around flex-wrap gap-6 shadow-md p-8 rounded-lg animate-fade-up opacity-0 mx-6"
-              style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
+              className="flex justify-around flex-wrap gap-6 shadow-md p-8 rounded-lg mx-6"
             >
               {brandDesign.map(person =>
                 <Card
@@ -153,7 +132,6 @@ export default function OrganizingTeamPage() {
             </div>
           </div>
         </div>
-      </div>
     </div>
   )
 }
--- apps/front/app/components/home/second-section.tsx
+++ apps/front/app/components/home/second-section.tsx
@@ -12,7 +12,7 @@ const SecondSection = () => {
       <div
         className="md:flex w-full text-sm max-w-screen-xl my-8"
       >
-        <div className="w-full space-y-4 p-4 xl:p-0">
+        <div className="w-full space-y-4 p-4 xl:p-0 xl:pl-[15%]">
           <p className="md:w-3/4">Le &apos;Moroccan Day of Mathematics&apos; (MDM) est un événement dédié aux passionnés de mathématiques, comprenant plusieurs compétitions et activités enrichissantes</p>
           <p className="md:w-3/4">Pendant cet événement, les participants pourront s'affronter dans plusieurs compétitions selon leurs niveaux. En plus des compétitions, il y aura des conférences et des stands pour explorer différentes facettes des mathématiques</p>
           <Button disabled variant='default'>
@@ -20,7 +20,7 @@ const SecondSection = () => {
           </Button>
         </div>
 
-        <div className="w-full space-y-4 font-medium text-4xl p-4 xl:p-0">
+        <div className="w-full space-y-4 font-medium text-4xl p-4 xl:p-0 xl:pl-[5%]">
           <p><span className='bg-gradient-to-br from-sky-500 to-[#272162] inline-block text-transparent bg-clip-text'>200+</span> Participants</p>
           <p><span className='bg-gradient-to-br from-sky-500 to-[#272162] inline-block text-transparent bg-clip-text'>3</span> Speakers</p>
           <p><span className='bg-gradient-to-br from-sky-500 to-[#272162] inline-block text-transparent bg-clip-text'>1</span> journée prise en charge</p>
