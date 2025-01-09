9e86edbc8cd0d8d75fa9f2d954fa3faf1ee69644
Achraf <achrafelkhamsi@gmail.com>
2025-01-09 13:18:15 +0100

    update faq page

--- apps/front/app/(pages)/(static)/faq/page.tsx
+++ apps/front/app/(pages)/(static)/faq/page.tsx
@@ -14,17 +14,17 @@ export default function ConferencesPage() {
         </h1>
 
         <p
-          className="animate-fade-up bg-clip-text text-center font-display text-sm opacity-0"
+          className="animate-fade-up bg-clip-text text-center font-display opacity-0"
           style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
         >
           Vous avez une question ? Veuillez lire les questions-réponses sur cette FAQ où vous allez trouver les réponses aux questions les plus posées par les participants de l&apos;édition précédente.
         </p>
 
         <p
-          className="animate-fade-up bg-clip-text text-center font-display text-sm opacity-0"
+          className="animate-fade-up bg-clip-text text-center font-display opacity-0"
           style={{ animationDelay: "0.20s", animationFillMode: "forwards" }}
         >
-          Pour toute autre question non traitée sur le site vous pouvez nous contacter, via nos réseaux sociaux ou par email !<br/>
+          Pour toute autre question non traitée sur le site vous pouvez nous contacter via nos réseaux sociaux ou par email<br/>
           Notez cependant que nous ne pourrons répondre qu&apos;aux questions non discutées sur le site.
         </p>
 
@@ -33,13 +33,13 @@ export default function ConferencesPage() {
           style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
         >
           <Tabs defaultValue="general">
-            <TabsList className="grid grid-cols-2 md:grid-cols-5">
+            {/* <TabsList className="grid grid-cols-2 md:grid-cols-5">
               <TabsTrigger value="general" className="text-sm">Général</TabsTrigger>
               <TabsTrigger value="math_sprint">Math Sprint</TabsTrigger>
               <TabsTrigger value="best_math_video">Best Math Video</TabsTrigger>
               <TabsTrigger value="stand">Stand</TabsTrigger>
               <TabsTrigger value="conferences">Conférences</TabsTrigger>
-            </TabsList>
+            </TabsList> */}
 
             <div className="mt-18 md:mt-0">
               <TabsContent value="general">
--- apps/front/app/(pages)/(static)/faq/questions.ts
+++ apps/front/app/(pages)/(static)/faq/questions.ts
@@ -1,13 +1,44 @@
 export const generalQuestions = [
   {
     question: "Qui peut s'inscrire ?",
-    anwser: "Les élèves en Tronc Commun/ 1ère Année Bac / 2ème Année Bac (Il n'y a pas de restriction d'âge tant que le candidat est un étudiant de lycée lors de sa candidature)"
+    anwser: "Toute personne intéressée par les mathématiques peut participer à cet événement."
   },
   {
-    question: "Quel est le délai d'inscription ?",
-    anwser: "La date limite est fixée au 22 Septembre à 23h59, heure du Maroc. N'attendez pas jusqu'au dernier jour pour soumettre votre candidature. Aucune exception ne sera accordée pour les candidatures tardives, quelles que soient les raisons invoquées, telles que l'oubli, des problèmes de connexion internet ou toute autre excuse."
-  }
+    question: "Quel est le délai d’inscription ?",
+    anwser: "La date limite est fixée au 01/03 N'attendez pas jusqu'au dernier jour pour soumettre votre candidature. Aucune exception ne sera accordée pour les candidatures tardives, quelles que soient les raisons invoquées, telles que l'oubli, des problèmes de connexion internet ou toute autre excuse."
+  },
+  {
+    question: "Y-a-t-il des frais à payer pour la nourriture, et les activités ?",
+    anwser: "Non, tous les frais de nourriture et d'activités sont pris en charge pour les participants tout au long de l'événement. Seuls les frais de transport sont à leur charge."
+  },
+  {
+    question: "Est-ce que nos parents peuvent nous accompagner pendant l’événement ?",
+    anwser: "Oui, vous pouvez être accompagnés par vos parents pendant l’événement."
+  },
+  {
+    question: "Où pourrais-je vous contacter si j’ai une demande spéciale, ou une autre question ?",
+    anwser: "Vous pouvez envoyer un email à la boîte mail suivante: mdm.math.maroc@gmail.com"
+  },
+
+
+  {
+    question: "Pourrais-je participer même si je n’ai pas encore de carte d’identité nationale ?",
+    anwser: "Oui, il suffit de mettre une preuve d’identité qui contient une photo; exemples: attestation de scolarité, carte étudiant, carte de club, carte de fondation…"
+  },
+  {
+    question: "Faut-il une lettre de motivation lors de l’inscription ?",
+    anwser: "Non, les lettres de motivation ne seront pas prises en considération. Par contre dans le formulaire d’inscription il sera demandé d’inclure une motivation de max 300 mots."
+  },
+  {
+    question: "Que faire si je n’ai pas encore reçu mon relevé de notes ?",
+    anwser: "Normalement, vous avez tous le bulletin de l’année dernière, si vous l’avez perdu il est de votre responsabilité de solliciter votre école pour une copie. En cas de preuve sérieuse on peut accepter un bulletin d’une année précédente."
+  },
+  {
+    question: "Quelle langue doit être utilisée lors de la journée ?",
+    anwser: "Vous êtes libre d’utiliser n’importe quelle langue pendant les préparations. Par contre les rendus doivent être en français ou anglais.  "
+  },
 ]
+
 
 export const mathSprintQuestions = [
   {
