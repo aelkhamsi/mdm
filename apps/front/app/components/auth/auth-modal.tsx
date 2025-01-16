1aef4e51cc931e066a1ced325c4986c88c82a09d
Achraf <achrafelkhamsi@gmail.com>
2025-01-16 12:02:43 +0100

    change dark background color in homepage & change header logo

--- apps/front/app/components/auth/auth-modal.tsx
+++ apps/front/app/components/auth/auth-modal.tsx
@@ -34,7 +34,7 @@ const AuthModal = ({
         <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-6 pt-8 text-center md:px-16">
           <a href="/">
             <Image
-              src="/logo.png"
+              src="/mdm_logo_header.svg"
               alt="Precedent logo"
               width={35}
               height={35}
