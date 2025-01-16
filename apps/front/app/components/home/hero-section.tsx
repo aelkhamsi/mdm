434e5b2243a2c6f2935aab990a57071f703c7696
Achraf <achrafelkhamsi@gmail.com>
2025-01-16 11:27:58 +0100

    update layout of hero section & change cta button

--- apps/front/app/components/home/hero-section.tsx
+++ apps/front/app/components/home/hero-section.tsx
@@ -34,19 +34,18 @@ const HeroSection = () => {
 
         <div className='space-y-2'>
           <h1
-            className="animate-fade-up opacity-0 text-center text-[#131316] font-display text-4xl font-bold tracking-[0.05em] text-wrap:balance md:text-5xl md:leading-[3rem]"
+            className="animate-fade-up opacity-0 text-center font-display text-4xl font-bold tracking-[0.05em] text-wrap:balance md:text-5xl md:leading-[3rem]"
             style={{ animationDelay: "0.20s", animationFillMode: "forwards" }}
           >
-            Le plus grand événement de mathématiques au Maroc
+            <span className='bg-gradient-to-br from-[#292660] to-stone-700 inline-block text-transparent bg-clip-text'>Le plus grand événement de mathématiques au Maroc</span>
           </h1>
 
-          <p
+          {/* <p
             className="animate-fade-up opacity-0 text-center text-gray-500 text-wrap:balance"
             style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
           >
             Explorer le riche patrimoine, les innovations modernes et le potentiel futur des mathématiques au Maroc
-            {/* Exploring the Rich Heritage, Modern Innovations, and Future Potential of Mathematics in Morocco. */}
-          </p>
+          </p> */}
         </div>
 
         <div
@@ -54,20 +53,22 @@ const HeroSection = () => {
           style={{ animationDelay: "0.30s", animationFillMode: "forwards" }}
         >
           <Link
-            className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-4 py-2 text-gray-600 shadow-md transition-colors hover:border-gray-800"
             href="/application"
           >
-            <Scroll className='h-4 w-4'/>
-            <p>
-              <span className="hidden sm:inline-block"></span> Participer
-            </p>
+            <button className="relative inline-flex h-11 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+              <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+              <span className="inline-flex h-full w-full gap-x-2 cursor-pointer items-center justify-center rounded-full bg-white px-4 py-1 text-black backdrop-blur-3xl">
+                <Scroll className='h-5 w-5'/>
+                Participer
+              </span>
+            </button>
           </Link>
 
           <Link
-            className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-4 py-2 text-gray-600 shadow-md transition-colors hover:border-gray-800"
+            className="flex max-w-fit items-center justify-center h-11 space-x-2 rounded-full border border-gray-300 bg-white px-4 py-2 text-gray-700 shadow-md transition-colors hover:border-gray-800"
             href="/faq"
           >
-            <MessageCircleIcon className='h-4 w-4'/>
+            <MessageCircleIcon className='h-5 w-5'/>
             <p>
               <span className="hidden sm:inline-block"></span> FAQ
             </p>
