6ae66c9bbca141a4a0c9c638da9d521452291839
Achraf <achrafelkhamsi@gmail.com>
2025-01-28 22:39:05 +0100

    add cta section in math sprint page

--- apps/front/app/(pages)/(static)/math-sprint/page.tsx
+++ apps/front/app/(pages)/(static)/math-sprint/page.tsx
@@ -1,3 +1,4 @@
+import CtaSection from "@/app/components/cta-section"
 import DivCircuitBg from "@/app/components/div-circuit-bg"
 import { ImageStack } from "@/app/components/image-stack"
 
@@ -16,16 +17,15 @@ export default function MathSprintPage() {
 
   return (
     <DivCircuitBg classNameBgImage="opacity-50 -z-[8]">
-      <div className="w-full max-w-sm md:max-w-screen-xl px-5 xl:px-0 space-y-14">
+      <div className="w-full flex flex-col items-center max-w-sm md:max-w-screen-xl px-5 xl:px-0 space-y-10 mb-20">
         <h1 className="text-center text-4xl font-bold drop-shadow-sm">
-          <span className='bg-gradient-to-br from-sky-800 to-[#272162] inline-block text-transparent bg-clip-text'>Math Sprint</span>
+          <span className='bg-gradient-to-br from-sky-600 to-[#1C55FF] inline-block text-transparent bg-clip-text'>Math Sprint</span>
         </h1>
 
         <div
           className="flex flex-col w-full md:flex-row md:justify-between md:space-x-8 text-base"
         >
           <div className="w-full space-y-4 p-4">
-            <p className='font-bold text-xs text-cyan-600'>C'est quoi Math Sprint</p>
             <h1 className='font-bold text-3xl'>Une compétition pour les esprits mathématiques affûtés</h1>
             <p className="text-lg">C&apos;est une compétition mathématique individuelle d&apos;une heure sous forme de QCM</p>
             <p className="text-lg">Pour cette année, on a <span className="font-bold">2 nouveautés</span>:
@@ -48,17 +48,19 @@ export default function MathSprintPage() {
           className="flex flex-col w-full md:flex-row md:justify-between md:space-x-8 text-base"
         >
           <div className="w-full space-y-4 p-4">
-            <p className='font-bold text-xs text-cyan-600'>La séléction</p>
+            <p className='font-bold text-sm text-[#1C55FF]'>La séléction</p>
+            <h1 className='font-bold text-3xl'>Comment se passera la selection ?</h1>
             <p className="text-lg">Avant de pouvoir participer à cette compétition, un test de sélection sera mené pour évaluer le niveau des potentiels participants</p>
-            <p className="text-lg">
-              La procédure retenue sera choisie ultérieurement parmi les options suivantes:
-              <ul className="list-disc">
-                <li><span className="font-bold">Plateforme en ligne: </span> Le test sera effectué sur une plateforme en ligne sous la forme d'un QCM surveillé par caméra</li>
-                <li><span className="font-bold">Entretien individuel: </span> Les candidats vont être contactés pour passer un entretien individuel en ligne. Les examinateurs évalueront la capacité à résoudre des questions de logique et de mathématiques</li>
-              </ul>
-            </p>
+            <p className="text-lg">Le test sera effectué sur une <span className="font-semibold text-gray-800">plateforme en ligne</span> sous la forme d'un QCM surveillé par caméra</p>
+          </div>
+
+          <div className="w-full space-y-4 p-4">
+            <p className='font-bold text-sm text-[#1C55FF]'>Le sylabus</p>
+            <h1 className='font-bold text-3xl'>Quels sujets concernent l'épreuve ?</h1>
           </div>
         </div>
+
+        <CtaSection />
       </div>
     </DivCircuitBg>
   )
