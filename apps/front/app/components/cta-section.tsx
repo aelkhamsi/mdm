9e9ab0a5020931dad1c26fdbf23223c81a12ab9a
Achraf <achrafelkhamsi@gmail.com>
2025-01-28 21:51:41 +0100

    update cta section in best math video page

---
+++ apps/front/app/components/cta-section.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+import CtaButton from './home/cta-button'
+import FaqButton from './home/faq-button'
+import './cta-section.css'
+
+const CtaSection = () => {
+  return (
+    <div className="pattern text-center w-1/2 p-10 space-y-8 text-white custom-shadow">
+      <div className='space-y-2'>
+        <h2 className='font-bold text-3xl'>Rejoindre l&apos;aventure</h2>
+        <p className="text-base">Laissez-vous inspirer par la beauté des mathématiques</p>    
+      </div>
+      
+      <div className='flex justify-center space-x-4'>
+        <CtaButton />
+        <FaqButton />
+      </div>
+    </div>
+  )
+}
+
+export default CtaSection
