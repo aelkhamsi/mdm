29c2a4a37a0f3b1fb3cc6418006e59562c6fc3c9
Achraf <achrafelkhamsi@gmail.com>
2025-02-02 12:23:59 +0100

    update parental auhorization link

--- apps/front/app/(pages)/application/form/steps/upload-step.tsx
+++ apps/front/app/(pages)/application/form/steps/upload-step.tsx
@@ -146,7 +146,7 @@ export const UploadStep = ({
 
                   return (
                     <FormItem>
-                      <FormLabel>Autorisation parentale signée et légalisée par le tuteur légal (<Link className="text-blue-500 underline" href='https://drive.google.com/file/d/19I9hXIptNqAL_RDT6C1m2uA5FFok-Lmd/view?usp=drive_link' target="_blank">fichier</Link>)<RequiredAsterisk /></FormLabel>
+                      <FormLabel>Autorisation parentale signée et légalisée par le tuteur légal (<Link className="text-blue-500 underline" href='https://drive.google.com/file/d/1gF5avT-WrBoUxD1DoCi4f3Hgk-OCiJyV/view?usp=sharing' target="_blank">fichier</Link>)<RequiredAsterisk /></FormLabel>
                       <FormControl>
                         <Input
                           {...form.register("fileParentalAuthorization", {
@@ -232,7 +232,7 @@ export const UploadStep = ({
 
                   return (
                     <FormItem>
-                      <FormLabel>Autorisation parentale signée et légalisée par le tuteur légal (<Link className="text-blue-500 underline" href='https://drive.google.com/file/d/19I9hXIptNqAL_RDT6C1m2uA5FFok-Lmd/view?usp=drive_link' target="_blank">fichier</Link>)<RequiredAsterisk /></FormLabel>
+                      <FormLabel>Autorisation parentale signée et légalisée par le tuteur légal (<Link className="text-blue-500 underline" href='https://drive.google.com/file/d/1gF5avT-WrBoUxD1DoCi4f3Hgk-OCiJyV/view?usp=sharing' target="_blank">fichier</Link>)<RequiredAsterisk /></FormLabel>
                       <FormControl>
                         <Input
                           {...form.register("fileParentalAuthorization", {
