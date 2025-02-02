1210d44bed063453334388321dbd3a817e80294b
Achraf <achrafelkhamsi@gmail.com>
2025-02-03 00:22:08 +0100

    checks if application status in not null when updating an application

--- apps/api/src/modules/application/controllers/application.controller.ts
+++ apps/api/src/modules/application/controllers/application.controller.ts
@@ -99,7 +99,7 @@ export class ApplicationController {
       );
 
       const applicationStatus = application?.status;
-      if (applicationStatus.status === 'NOTIFIED') {
+      if (applicationStatus && applicationStatus.status === 'NOTIFIED') {
         this.applicationStatusService.update(applicationStatus?.id, {
           ...applicationStatus,
           status: 'UPDATED',
