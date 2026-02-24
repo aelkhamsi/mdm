44f19bfadee47893005e473adf3e4b9cdfce4be3
Achraf <achrafelkhamsi@gmail.com>
2026-02-24 15:39:08 +0100

    add video playing in place of homepage logogit add .

--- apps/front/app/components/homepage/hero-section/hero-section-logo.tsx
+++ apps/front/app/components/homepage/hero-section/hero-section-logo.tsx
@@ -1,16 +1,38 @@
-import Image from "next/image"
+"use client"
+
+import { useRef } from "react";
 
 const HeroSectionLogo = () => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+
   return (
     <div 
-      className="animate-fade-up opacity-0 z-10 p-4 md:p-0"
+      className="relative animate-fade-up opacity-0 flex items-center z-10 p-4 md:p-0 h-[12rem] sm:h-[15rem] md:h-[18rem] xl:h-[22rem]"
       style={{ animationDelay: "0.20s", animationFillMode: "forwards" }}
     >
-      <Image
-        src="/images/logos/primary_logo.svg"
+      <video
+        ref={videoRef}
+        autoPlay
+        muted
+        playsInline
+        onEnded={() => {
+          const video = videoRef.current;
+          if (!video) return;
+          video.currentTime = 0;
+
+          setTimeout(() => {
+            video.play();
+          }, 7000);
+        }}
+        className="relative inset-0 h-full object-cover"
+      >
+        <source src="/videos/mdm_2.mp4" type="video/mp4" />
+      </video>
+
+      <img
+        src="/images/logos/primary_logo_text.png"
         alt="MDM logo"
-        width='570'
-        height='200'
+        className="relative h-1/2"
       />
     </div>
   )
--- apps/front/app/components/homepage/hero-section/index.tsx
+++ apps/front/app/components/homepage/hero-section/index.tsx
@@ -13,22 +13,24 @@ const HeroSection = () => {
   return (
     <CenterContainer className={`pt-28 pb-10 z-0 space-y-10 ${backgroundColorClassname}`}>
       
-      <div className='flex flex-col items-center space-y-16'>
+      <div className='flex flex-col items-center'>
         <HeroSectionLogo />
 
-        <HeroSectionTitle />
+        <div className='flex flex-col gap-y-16'>
+          <HeroSectionTitle />
 
-        <div
-          className="mx-auto flex flex-wrap gap-y-4 animate-fade-up items-center justify-center space-x-5 opacity-0"
-          style={{ animationDelay: "0.30s", animationFillMode: "forwards" }}
-        >
-          <CtaButton />
-          <DeadlineButton />
-          <RegulationButton />
-          <FaqButton />
+          <div
+            className="mx-auto flex flex-wrap gap-y-4 animate-fade-up items-center justify-center space-x-5 opacity-0"
+            style={{ animationDelay: "0.30s", animationFillMode: "forwards" }}
+          >
+            <CtaButton />
+            <DeadlineButton />
+            <RegulationButton />
+            <FaqButton />
+          </div>
+
+          <HeroSectionPartners />
         </div>
-
-        <HeroSectionPartners />
       </div>
       
     </CenterContainer>
